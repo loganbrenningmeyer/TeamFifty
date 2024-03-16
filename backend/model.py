790186ae3c3b3be4a6e536c98ff635a5ca08dcfa
Logan Brenningmeyer <loganbrenningmeyer@gmail.com>
2024-03-16 21:12:51 +0000

    model is trainable

---
+++ backend/model.py
@@ -0,0 +1,33 @@
+import players
+import numpy as np
+import torch
+import ANN
+
+torch.set_printoptions(threshold=torch.inf)
+
+league = 1
+season = 2023
+
+# Returns a dictionary of data for each game in a given league/season
+# Dictionary in the format: {game_name: {'input': input_data, 'target': target_data}}
+input_data, target_data = players.get_pytorch_data(league, season)
+
+print(input_data, target_data)
+print(input_data.shape)
+print(target_data.shape)
+
+# Create a model with the same number of inputs and outputs as the data
+input_size = input_data.shape[1]
+output_size = 1
+
+# Create the model
+model = ANN.ANN(input_size, output_size, 3, [32, 16, 8], [1, 1, 1])
+
+# Train the model
+model.train(input_data, target_data, 100, 10, 0.01)
+
+# Predict the output
+for input, target in zip(input_data, target_data):
+    print(model.predict(input))
+    print(target)
+
