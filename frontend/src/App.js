c50f753b759ed4d56ab55e1305c2bf409b5aa5b1
loganbrenningmeyer <loganbrenningmeyer@gmail.com>
2024-03-21 13:28:01 +0000

    react test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './components/About';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Model from './components/Model/Model';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<About/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/model" element={<Model/>} />
       </Routes>
     </Router>
   );
