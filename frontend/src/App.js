48bfb286fb2ea0d9bbaeddd1b9554bf177b7ffef
cvanmeter-rl <christian.vanmeter@yahoo.com>
2024-04-10 19:17:44 -0500

    dashboard page
    dashboard page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About/About';
@@ -7,13 +7,14 @@ import Home from './components/Home/Home';
 import Model from './components/Model/Model';
 import Data from './components/Data/Data';
 import Parameters from './components/Parameters/Parameters';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/about" element={<About/>} />
-        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/" element={<Dashboard/>} />
         <Route exact path="/model" element={<Model/>} />
         <Route exact path="/data" element={<Data/>} />
         <Route exact path="/parameters" element={<Parameters/>} />
