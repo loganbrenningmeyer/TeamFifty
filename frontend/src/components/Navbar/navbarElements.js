13e8a3053a4983b6df9860b3bcfb7c61a2b4827e
JoshJaws <164087274+JoshuaJaworski@users.noreply.github.com>
2024-04-28 22:31:02 -0500

    Design Changes
    Changed colors and some formatting.
    Copied svg images in but need to implement still.

--- frontend/src/components/Navbar/navbarElements.js
+++ frontend/src/components/Navbar/navbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  background: #000;
+  background: #23272D;
   height: 80px;
   display: flex;
   justify-content: center;
