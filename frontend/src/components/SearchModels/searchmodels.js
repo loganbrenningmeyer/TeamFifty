e9213a10670485c1dc7e222f0543d4b909a62863
cvanmeter-rl <christian.vanmeter@yahoo.com>
2024-04-28 23:12:23 -0500

    updated search models page
    fixed issue where search results wouldnt change from previous ones if the next search didn't result in any models being found.

--- frontend/src/components/SearchModels/searchmodels.js
+++ frontend/src/components/SearchModels/searchmodels.js
@@ -34,77 +34,111 @@ function SearchModels(){
     const [activeValidationData, setActiveValidationData] = useState({});
 
     const handleSearch = async () => {
-        try{
-        
-        const response = await axios.get('http://localhost:3000/search',
-        {params: {q:searchQuery}
-    });
-        if (response.data.length > 0){
-            setSearchResults(response.data);
-            //console.log(searchQuery);
-            setActiveModelName(response.data[0].model_name);
-            setActiveModelType(response.data[0].model_type);
-            //setActiveModelVis(response.data[0].model_vis);
+        if(searchQuery.length !== 0)
+        {
+            try{
+            
+            const response = await axios.get('http://localhost:3000/search',
+            {params: {q:searchQuery}
+        });
+            if (response.data.length > 0){
+                setSearchResults(response.data);
+                console.log(searchQuery);
+                setActiveModelName(response.data[0].model_name);
+                setActiveModelType(response.data[0].model_type);
+                //setActiveModelVis(response.data[0].model_vis);
 
-            //console.log(response.data[0].training_loss)
-            //console.log(response.data[0].training_accuracy)
-            /* Set training loss data */
-          const training_data = {
-            labels: Array.from(Array(100).keys()),
-            datasets: [
-              {
-                label: 'Training Loss',
-                data: response.data[0].training_loss,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)'
-              },
-              {
-                label: 'Training Accuracy',
-                data: response.data[0].training_accuracy,
-                borderColor: 'rgb(0, 204, 102)',
-                backgroundColor: 'rgba(0, 204, 102, 0.5)'
-              }
-            ]
-          }
+                //console.log(response.data[0].training_loss)
+                //console.log(response.data[0].training_accuracy)
+                /* Set training loss data */
+            const training_data = {
+                labels: Array.from(Array(100).keys()),
+                datasets: [
+                {
+                    label: 'Training Loss',
+                    data: response.data[0].training_loss,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)'
+                },
+                {
+                    label: 'Training Accuracy',
+                    data: response.data[0].training_accuracy,
+                    borderColor: 'rgb(0, 204, 102)',
+                    backgroundColor: 'rgba(0, 204, 102, 0.5)'
+                }
+                ]
+            }
 
-          /* Set validation loss data */
-          const validation_data = {
-            labels: Array.from(Array(100).keys()),
-            datasets: [
-              {
-                label: 'Validation Loss',
-                data: response.data[0].validation_loss,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)'
-              },
-              {
-                label: 'Validation Accuracy',
-                data: response.data[0].validation_accuracy,
-                borderColor: 'rgb(0, 204, 102)',
-                backgroundColor: 'rgba(0, 204, 102, 0.5)'
-              }
-            ]
-          }
+            /* Set validation loss data */
+            const validation_data = {
+                labels: Array.from(Array(100).keys()),
+                datasets: [
+                {
+                    label: 'Validation Loss',
+                    data: response.data[0].validation_loss,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)'
+                },
+                {
+                    label: 'Validation Accuracy',
+                    data: response.data[0].validation_accuracy,
+                    borderColor: 'rgb(0, 204, 102)',
+                    backgroundColor: 'rgba(0, 204, 102, 0.5)'
+                }
+                ]
+            }
 
-          setActiveTrainingData(training_data);
-          setActiveValidationData(validation_data);
+            setActiveTrainingData(training_data);
+            setActiveValidationData(validation_data);
 
-          
+            
 
 
+            }
+            else
+            {
+                setSearchResults([])
+            }
+
+            console.log(response.data);
+            } catch (error){
+                console.error(error);
+                setSearchResults([])
+            };
         }
-
-        console.log(response.data);
-        } catch (error){
-            console.error(error);
-            setSearchResults([])
-        };
     };
+
+    let info = () => {
+        if(searchResults.length === 0)
+        {
+            return( <p>No Models Were Found</p>)
+        }
+        return(
+        searchResults.map((element,index) =>{
+            return(
+        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {element.model_name}
+            </th>
+            <td class="px-6 py-4">
+            {element.model_type}
+            </td>
+            <td class="px-6 py-4">
+            {element.validation_accuracy[99]}
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+            </td>
+            </tr>
+            )})
+        )}
+    
 
     return(
         <div>
             <h1>Model Search</h1>
             <input
+            required
             type="text"
             placeholder="Search models..."
             onChange={(e) => setSearchQuery(e.target.value)}
@@ -136,24 +170,7 @@ function SearchModels(){
                     </thead>
                     <tbody>
                     {
-                        searchResults.length === 0 ? <p>No Models Were Found</p> :
-                            searchResults.map((element,index) =>{
-                                return(
-                            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {element.model_name}
-                                </th>
-                                <td class="px-6 py-4">
-                                {element.model_type}
-                                </td>
-                                <td class="px-6 py-4">
-                                {element.validation_accuracy[99]}
-                                </td>
-                                <td class="px-6 py-4">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
-                                </td>
-                            </tr>
-                                )})
+                    info()
                     }
                     
                     </tbody>
